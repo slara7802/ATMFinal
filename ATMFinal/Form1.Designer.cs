fe8429d409ba31126b130232ac704e331fc344b3
slara7802 <102179400+slara7802@users.noreply.github.com>
2022-03-22 12:00:18 -0600

    Add files via upload

---
+++ ATMFinal/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace ATMFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UserNameBox = new System.Windows.Forms.TextBox();
+            this.PasswordBox = new System.Windows.Forms.TextBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UserNameBox
+            // 
+            this.UserNameBox.Location = new System.Drawing.Point(77, 90);
+            this.UserNameBox.Name = "UserNameBox";
+            this.UserNameBox.Size = new System.Drawing.Size(137, 20);
+            this.UserNameBox.TabIndex = 0;
+            // 
+            // PasswordBox
+            // 
+            this.PasswordBox.Location = new System.Drawing.Point(77, 149);
+            this.PasswordBox.Name = "PasswordBox";
+            this.PasswordBox.Size = new System.Drawing.Size(137, 20);
+            this.PasswordBox.TabIndex = 1;
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(117, 200);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(75, 23);
+            this.loginBtn.TabIndex = 2;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 335);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.PasswordBox);
+            this.Controls.Add(this.UserNameBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UserNameBox;
+        private System.Windows.Forms.TextBox PasswordBox;
+        private System.Windows.Forms.Button loginBtn;
+    }
+}
+
---
+++ ATMFinal/sqlQueries.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMFinal
+{
+    class sqlQueries
+    {
+        private SqlConnection con = new SqlConnection("server=localhost;database=AccountingTracker;UID=sa;password=123456789");
+
+
+        //--------------------------------INSERT QUERIES-----------------------------------------------
+        public void InsertInto(string UserName, string Password, string FirstName, string LastName, string TotalBalance)
+        {
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO UserInfo (UserName, Password, FirstName, LastName, TotalBalance) VALUES (@UserName, @Password, @FirstName, @LastName. @TotalBalance)", con);
+            cmd.Parameters.AddWithValue("@UserName", UserName);
+            cmd.Parameters.AddWithValue("@Password", Password);
+            cmd.Parameters.AddWithValue("@FirstName", FirstName);
+            cmd.Parameters.AddWithValue("@LastName", LastName);
+            cmd.Parameters.AddWithValue("@TotalBalance", TotalBalance);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public void ExpenseInsert(double amount, DateTime date, string description, string category, int userId)
+        {
+            SqlCommand cmd = new SqlCommand("INSERT INTO Expense (ExAmount, ExDate, ExDescription, ExCategory, UserId) VALUES (@Amount, @date, @description, @category, @userId)", con);
+            cmd.Parameters.AddWithValue("@Amount", amount);
+            cmd.Parameters.AddWithValue("@date", date);
+            cmd.Parameters.AddWithValue("@description", description);
+            cmd.Parameters.AddWithValue("@category", category);
+            cmd.Parameters.AddWithValue("@userId", userId);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+
+
+
+        //-------------------------------SELECT QUERIES-------------------------------------------------
+
+        public bool SelectUser(string userName)
+        {
+
+            SqlCommand cmd2 = new SqlCommand("select * from UserInfo where UserName=@UserName", con);
+            cmd2.Parameters.AddWithValue("@UserName", userName);
+
+            SqlDataAdapter sda = new SqlDataAdapter(cmd2);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            con.Open();
+            if (dt.Rows.Count > 0)
+            {
+                con.Close();
+
+                return true;
+            }
+            else
+            {
+                con.Close();
+                return false;
+            }
+
+
+
+        }
+
+        public DataTable IdentifierUser(string userName, string password)
+        {
+
+            SqlCommand cmd2 = new SqlCommand("select * from UserInfo where UserName=@UserName AND Password=@Password", con);
+            cmd2.Parameters.AddWithValue("@UserName", userName);
+            cmd2.Parameters.AddWithValue("@Password", password);
+
+            SqlDataAdapter sda = new SqlDataAdapter(cmd2);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            con.Open();
+            if (dt.Rows.Count > 0)
+            {
+                con.Close();
+
+                return dt;
+            }
+            else
+            {
+                con.Close();
+                return dt;
+            }
+
+
+
+        }
+
+        public DataTable GetExpenses(int UserId)
+        {
+            SqlCommand cmd2 = new SqlCommand("select ExAmount AS Amount,ExDate As Date, ExDescription As Description, ExCategory AS Category  from Expense where UserId =@userId", con);
+            cmd2.Parameters.AddWithValue("@userId", UserId);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd2);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            con.Open();
+            con.Close();
+            return dt;
+
+
+        }
+    }
+}
